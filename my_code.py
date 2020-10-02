51da4b67863f167ff8f22c1c2ebbcd1a03171c51
parkerS123 <23pschwantes@athenian.org>
2020-10-02 20:31:03 +0000

    start of code

--- my_code.py
+++ my_code.py
@@ -1,7 +1,19 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
 
+answer1 = ["Pd4", "Qd3", "Qh3", "Qc8"] 
+print(answer1)
+answer2 = ["Pc4", "Qa4", "Qc6", "Qc8"]
+print(answer2)
 
+print("There are 2 ways to beat the copycat in chess in 4 moves. Try to find the right combination and you'll win.")
+print("To enter moves, Use the first letter of the piece first (capital letter), then the sqaure that piece is moving to.")
+
+practice = str(input("Try entering bishop to b4: "))
+if practice == "Bb4":
+    print("You got it! ")
+else:
+    print("The correct input was: Bb4")
 
 
 
